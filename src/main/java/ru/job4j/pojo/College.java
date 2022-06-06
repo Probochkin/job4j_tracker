e4e323f99b68fc668b0c5cf2d0e539f53c5416fc
Семен <semion.probochkin@yandex.ru>
2022-06-06 10:21:15 +0300

    Модель данных.

---
+++ src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,15 @@
+package ru.job4j.pojo;
+
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+public class College {
+    public static void main(String[] args) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        Student student = new Student();
+        student.setFullName("Иванов Петр Иванович");
+        student.setDateEntry(new Date());
+        student.setGroup("13ВЭ2");
+        System.out.println("Студент" + student.getFullName() + " группы " +student.getGroup() +" поступил " + student.getDateEntry());
+    }
+}
